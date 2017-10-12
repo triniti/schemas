3ce68d76965955dd699e576053949cc2a6b463b4
Arun Karnati <arun.karnati@gmail.com>
2017-10-11 17:42:49 -0700

    added pages schemas for different actions

---
+++ build/php/src/Triniti/Schemas/Pages/Mixin/GetPageRequest/GetPageRequestV1Mixin.php
@@ -0,0 +1,32 @@
+<?php
+// @link http://schemas.triniti.io/json-schema/triniti/pages/mixin/get-page-request/1-0-0.json#
+namespace Triniti\Schemas\Pages\Mixin\GetPageRequest;
+
+use Gdbots\Pbj\AbstractMixin;
+use Gdbots\Pbj\Enum\Format;
+use Gdbots\Pbj\FieldBuilder as Fb;
+use Gdbots\Pbj\SchemaId;
+use Gdbots\Pbj\Type as T;
+
+final class GetPageRequestV1Mixin extends AbstractMixin
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return SchemaId::fromString('pbj:triniti:pages:mixin:get-page-request:1-0-0');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFields()
+    {
+        return [
+            Fb::create('vanity_url', T\StringType::create())
+                ->format(Format::URL())
+                ->build(),
+        ];
+    }
+}
---
+++ build/php/src/Triniti/Schemas/Pages/Mixin/GetPageResponse/GetPageResponse.php
@@ -0,0 +1,9 @@
+<?php
+// @link http://schemas.triniti.io/json-schema/triniti/pages/mixin/get-page-response/latest.json#
+namespace Triniti\Schemas\Pages\Mixin\GetPageResponse;
+
+use Gdbots\Schemas\Ncr\Mixin\GetNodeResponse\GetNodeResponse;
+
+interface GetPageResponse extends GetNodeResponse
+{
+}
