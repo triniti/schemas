4243e21494a79d7674e9b5fbe3824d202ba49b79
Arun Karnati <arun.karnati@gmail.com>
2017-10-30 16:24:07 -0700

    status field added to page publish & unpublish schemas

--- build/php/src/Triniti/Schemas/Pages/Mixin/PageUnpublished/PageUnpublishedV1Mixin.php
+++ build/php/src/Triniti/Schemas/Pages/Mixin/PageUnpublished/PageUnpublishedV1Mixin.php
@@ -3,7 +3,10 @@
 namespace Triniti\Schemas\Pages\Mixin\PageUnpublished;
 
 use Gdbots\Pbj\AbstractMixin;
+use Gdbots\Pbj\FieldBuilder as Fb;
 use Gdbots\Pbj\SchemaId;
+use Gdbots\Pbj\Type as T;
+use Gdbots\Schemas\Ncr\Enum\NodeStatus;
 
 final class PageUnpublishedV1Mixin extends AbstractMixin
 {
@@ -13,5 +16,18 @@ final class PageUnpublishedV1Mixin extends AbstractMixin
     public function getId()
     {
         return SchemaId::fromString('pbj:triniti:pages:mixin:page-unpublished:1-0-0');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFields()
+    {
+        return [
+            Fb::create('status', T\StringEnumType::create())
+                ->withDefault(NodeStatus::DRAFT())
+                ->className(NodeStatus::class)
+                ->build(),
+        ];
     }
 }
