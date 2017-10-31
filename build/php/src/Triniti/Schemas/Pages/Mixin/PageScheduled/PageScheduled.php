f8ffe449f9648b48e8c21d6150cbd8d70712d49a
Arun Karnati <arun.karnati@gmail.com>
2017-10-31 10:12:50 -0700

    page scheduled mixin added and page node mixin updated

---
+++ build/php/src/Triniti/Schemas/Pages/Mixin/PageScheduled/PageScheduled.php
@@ -0,0 +1,9 @@
+<?php
+// @link http://schemas.triniti.io/json-schema/triniti/pages/mixin/page-scheduled/latest.json#
+namespace Triniti\Schemas\Pages\Mixin\PageScheduled;
+
+use Gdbots\Schemas\Ncr\Mixin\NodeScheduled\NodeScheduled;
+
+interface PageScheduled extends NodeScheduled
+{
+}
---
+++ build/php/src/Triniti/Schemas/Pages/Mixin/PageScheduled/PageScheduledV1Mixin.php
@@ -0,0 +1,33 @@
+<?php
+// @link http://schemas.triniti.io/json-schema/triniti/pages/mixin/page-scheduled/1-0-0.json#
+namespace Triniti\Schemas\Pages\Mixin\PageScheduled;
+
+use Gdbots\Pbj\AbstractMixin;
+use Gdbots\Pbj\FieldBuilder as Fb;
+use Gdbots\Pbj\SchemaId;
+use Gdbots\Pbj\Type as T;
+use Gdbots\Schemas\Ncr\Enum\NodeStatus;
+
+final class PageScheduledV1Mixin extends AbstractMixin
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return SchemaId::fromString('pbj:triniti:pages:mixin:page-scheduled:1-0-0');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFields()
+    {
+        return [
+            Fb::create('status', T\StringEnumType::create())
+                ->withDefault(NodeStatus::DRAFT())
+                ->className(NodeStatus::class)
+                ->build(),
+        ];
+    }
+}
