2e4cc99922094d3db5b850656c80afb841f529e1
Arun Karnati <arun.karnati@gmail.com>
2017-11-08 17:57:52 -0800

    added slug field to publish page command

--- build/php/src/Triniti/Schemas/Pages/Mixin/PublishPage/PublishPageV1Mixin.php
+++ build/php/src/Triniti/Schemas/Pages/Mixin/PublishPage/PublishPageV1Mixin.php
@@ -3,7 +3,10 @@
 namespace Triniti\Schemas\Pages\Mixin\PublishPage;
 
 use Gdbots\Pbj\AbstractMixin;
+use Gdbots\Pbj\Enum\Format;
+use Gdbots\Pbj\FieldBuilder as Fb;
 use Gdbots\Pbj\SchemaId;
+use Gdbots\Pbj\Type as T;
 
 final class PublishPageV1Mixin extends AbstractMixin
 {
@@ -13,5 +16,17 @@ final class PublishPageV1Mixin extends AbstractMixin
     public function getId()
     {
         return SchemaId::fromString('pbj:triniti:pages:mixin:publish-page:1-0-0');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFields()
+    {
+        return [
+            Fb::create('slug', T\StringType::create())
+                ->format(Format::SLUG())
+                ->build(),
+        ];
     }
 }
