61d215992d19c023925a72f048a343f9e6a115b1
Josh Levy <jlevy.github@gmail.com>
2018-05-24 17:33:35 -0700

    Add credit field to curator:gallery node (#23)

--- build/php/src/Triniti/Schemas/Curator/Mixin/Gallery/GalleryV1Mixin.php
+++ build/php/src/Triniti/Schemas/Curator/Mixin/Gallery/GalleryV1Mixin.php
@@ -37,6 +37,12 @@ final class GalleryV1Mixin extends AbstractMixin
             Fb::create('image_ref', T\IdentifierType::create())
                 ->className(NodeRef::class)
                 ->build(),
+            /*
+             * A credit is a short string used to publicly acknowledge the source/creator
+             * of the photo.
+             */
+            Fb::create('credit', T\StringType::create())
+                ->build(),
             Fb::create('allow_comments', T\BooleanType::create())
                 ->withDefault(true)
                 ->build(),
