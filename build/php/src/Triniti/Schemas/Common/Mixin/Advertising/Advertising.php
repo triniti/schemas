e2ea5ce80d8f416f17ab3f370f2eea8b5f4912d7
albertgcsula <Alberto.Gutierrez@telepixtv.com>
2018-06-01 17:07:07 -0700

    add advertising mixin

---
+++ build/php/src/Triniti/Schemas/Common/Mixin/Advertising/Advertising.php
@@ -0,0 +1,9 @@
+<?php
+// @link http://schemas.triniti.io/json-schema/triniti/common/mixin/advertising/latest.json#
+namespace Triniti\Schemas\Common\Mixin\Advertising;
+
+use Gdbots\Pbj\Message;
+
+interface Advertising extends Message
+{
+}
