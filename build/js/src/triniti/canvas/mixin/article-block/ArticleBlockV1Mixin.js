17998099653aae75172034215f5fee27e59f0739
Eric Jacobson <contact@efjacobson.com>
2018-07-12 14:52:37 -0700

    compiled

--- build/js/src/triniti/canvas/mixin/article-block/ArticleBlockV1Mixin.js
+++ build/js/src/triniti/canvas/mixin/article-block/ArticleBlockV1Mixin.js
@@ -1,6 +1,9 @@
 // @link http://schemas.triniti.io/json-schema/triniti/canvas/mixin/article-block/1-0-0.json#
+import Fb from '@gdbots/pbj/FieldBuilder';
 import Mixin from '@gdbots/pbj/Mixin';
+import NodeRef from '@gdbots/schemas/gdbots/ncr/NodeRef';
 import SchemaId from '@gdbots/pbj/SchemaId';
+import T from '@gdbots/pbj/types';
 
 export default class ArticleBlockV1Mixin extends Mixin {
   /**
@@ -8,5 +11,23 @@ export default class ArticleBlockV1Mixin extends Mixin {
    */
   getId() {
     return SchemaId.fromString('pbj:triniti:canvas:mixin:article-block:1-0-0');
+  }
+
+  /**
+   * @returns {Field[]}
+   */
+  getFields() {
+    return [
+      Fb.create('link_text', T.StringType.create())
+        .build(),
+      Fb.create('show_image', T.BooleanType.create())
+        .build(),
+      /*
+       * An optional reference to an image asset to use as the poster.
+       */
+      Fb.create('image_ref', T.IdentifierType.create())
+        .classProto(NodeRef)
+        .build(),
+    ];
   }
 }
