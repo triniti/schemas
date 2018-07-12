94d496c0155fee35e9ad21782c0e3a11234c3a1c
Eric Jacobson <contact@efjacobson.com>
2018-07-11 18:57:53 -0700

    compiled

--- build/js/src/triniti/canvas/mixin/audio-block/AudioBlockV1Mixin.js
+++ build/js/src/triniti/canvas/mixin/audio-block/AudioBlockV1Mixin.js
@@ -1,6 +1,9 @@
 // @link http://schemas.triniti.io/json-schema/triniti/canvas/mixin/audio-block/1-0-0.json#
+import Fb from '@gdbots/pbj/FieldBuilder';
 import Mixin from '@gdbots/pbj/Mixin';
+import NodeRef from '@gdbots/schemas/gdbots/ncr/NodeRef';
 import SchemaId from '@gdbots/pbj/SchemaId';
+import T from '@gdbots/pbj/types';
 
 export default class AudioBlockV1Mixin extends Mixin {
   /**
@@ -8,5 +11,21 @@ export default class AudioBlockV1Mixin extends Mixin {
    */
   getId() {
     return SchemaId.fromString('pbj:triniti:canvas:mixin:audio-block:1-0-0');
+  }
+
+  /**
+   * @returns {Field[]}
+   */
+  getFields() {
+    return [
+      Fb.create('launch_text', T.StringType.create())
+        .build(),
+      /*
+       * An optional reference to an image asset to use as the poster.
+       */
+      Fb.create('image_ref', T.IdentifierType.create())
+        .classProto(NodeRef)
+        .build(),
+    ];
   }
 }
