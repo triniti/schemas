0d6e617acc244895a5a2a44cfae3528bbda29ac0
joel.capillo <joel.capillo@tmz.com>
2018-07-13 18:41:46 -0700

    finally

--- build/js/tests/triniti/sys/RedirectId.test.js
+++ build/js/tests/triniti/sys/RedirectId.test.js
@@ -1,7 +1,6 @@
 import test from 'tape';
 import RedirectId from '@triniti/schemas/triniti/sys/RedirectId';
 
-
 test('RedirectId tests[empty character will throw an error]', (t) => {
   t.throws(() => RedirectId.fromUri(''));
   t.end();
@@ -10,20 +9,22 @@ test('RedirectId tests[empty character will throw an error]', (t) => {
 
 test('RedirectId tests', (t) => {
   const urls = [
-    { output: 'cG9zdC9yZWluZGV4LyMvJCQlIQ', input: 'post/reindex/#/$$%!' },
-    { output: 'L3VuaWNvZGUvZmFrZS9wYXJ0aWFsL3VybA', input: '/unicode/fake/partial/url' },
-    { output: 'RVJUIUAjJCVeLw', input: 'ERT!@#$%^/' },
-    { output: 'aHR0cHM6Ly90ZXN0LmNvbS9BYUJjZEMxMjNmZzIzR1JyLw', input: 'https://test.com/AaBcdC123fg23GRr/' },
-    { output: 'aHR0cHM6Ly90ZXN0LmNvbS9hYmNkZWZnaHl1b2kvaGtmaGRmZHM', input: 'https://test.com/abcdefghyuoi/hkfhdfds' },
-    { output: 'JiYmJiYqKioqKioqLy8vLy8vXl5eXl5eXl4', input: '&&&&&*******//////^^^^^^^^' },
-    { output: 'L3BhZ2UvMS8yLw', input: '/page/1/2/' },
-    { output: 'cG9zdC8xMjMvIT1hQmNkRWZHPw', input: 'post/123/!=aBcdEfG?' },
+    { input: 'post/reindex/#/$$%!', output: 'cG9zdC9yZWluZGV4LyMvJCQlIQ' },
+    { input: '/unicode/fake/partial/url', output: 'L3VuaWNvZGUvZmFrZS9wYXJ0aWFsL3VybA' },
+    { input: 'ERT!@#$%^/', output: 'RVJUIUAjJCVeLw' },
+    { input: 'https://test.com/AaBcdC123fg23GRr/', output: 'aHR0cHM6Ly90ZXN0LmNvbS9BYUJjZEMxMjNmZzIzR1JyLw' },
+    { input: 'https://test.com/abcdefghyuoi/hkfhdfds', output: 'aHR0cHM6Ly90ZXN0LmNvbS9hYmNkZWZnaHl1b2kvaGtmaGRmZHM' },
+    { input: '&&&&&*******//////^^^^^^^^', output: 'JiYmJiYqKioqKioqLy8vLy8vXl5eXl5eXl4' },
+    { input: '/page/1/2/', output: 'L3BhZ2UvMS8yLw' },
+    { input: 'post/123/!=aBcdEfG?', output: 'cG9zdC8xMjMvIT1hQmNkRWZHPw' },
   ];
+
 
   urls.forEach((url) => {
     const id = RedirectId.fromUri(url.input);
     t.true(id instanceof RedirectId);
     t.same(url.output, id.toString(), 'same value');
+    t.same(url.input, id.toUri(), 'same value');
   });
 
   t.end();
--- build/php/tests/Triniti/Tests/Schemas/Sys/RedirectIdTest.php
+++ build/php/tests/Triniti/Tests/Schemas/Sys/RedirectIdTest.php
@@ -19,21 +19,20 @@ final class RedirectIdTest extends TestCase
     public function test(): void
     {
         $uris = [
-            (object) array('output' => 'cG9zdC9yZWluZGV4LyMvJCQlIQ', 'input' => 'post/reindex/#/$$%!'),
-            (object) array('output' => 'L3VuaWNvZGUvZmFrZS9wYXJ0aWFsL3VybA', 'input' => '/unicode/fake/partial/url'),
-            (object) array('output' => 'RVJUIUAjJCVeLw', 'input' => 'ERT!@#$%^/'),
-            (object) array('output' => 'aHR0cHM6Ly90ZXN0LmNvbS9BYUJjZEMxMjNmZzIzR1JyLw', 'input' => 'https://test.com/AaBcdC123fg23GRr/'),
-            (object) array('output' => 'aHR0cHM6Ly90ZXN0LmNvbS9hYmNkZWZnaHl1b2kvaGtmaGRmZHM', 'input' => 'https://test.com/abcdefghyuoi/hkfhdfds'),
-            (object) array('output' => 'JiYmJiYqKioqKioqLy8vLy8vXl5eXl5eXl4', 'input' => '&&&&&*******//////^^^^^^^^'),
-            (object) array('output' => 'L3BhZ2UvMS8yLw', 'input' => '/page/1/2/'),
-            (object) array('output' => 'cG9zdC8xMjMvIT1hQmNkRWZHPw', 'input' => 'post/123/!=aBcdEfG?'),
+            ['input' => 'post/reindex/#/$$%!', 'output' => 'cG9zdC9yZWluZGV4LyMvJCQlIQ'],
+            ['input' => '/unicode/fake/partial/url', 'output' => 'L3VuaWNvZGUvZmFrZS9wYXJ0aWFsL3VybA'],
+            ['input' => 'ERT!@#$%^/', 'output' => 'RVJUIUAjJCVeLw'],
+            ['input' => 'https://test.com/AaBcdC123fg23GRr/', 'output' => 'aHR0cHM6Ly90ZXN0LmNvbS9BYUJjZEMxMjNmZzIzR1JyLw'],
+            ['input' => 'https://test.com/abcdefghyuoi/hkfhdfds', 'output' => 'aHR0cHM6Ly90ZXN0LmNvbS9hYmNkZWZnaHl1b2kvaGtmaGRmZHM'],
+            ['input' => '&&&&&*******//////^^^^^^^^', 'output' => 'JiYmJiYqKioqKioqLy8vLy8vXl5eXl5eXl4'],
+            ['input' => '/page/1/2/', 'output' => 'L3BhZ2UvMS8yLw'],
+            ['input' => 'post/123/!=aBcdEfG?', 'output' => 'cG9zdC8xMjMvIT1hQmNkRWZHPw'],
         ];
 
         foreach ($uris as $uri) {
-            $id = RedirectId::fromUri($uri->input);
-            $this->assertSame($uri->output, $id->toString());
-            $this->assertSame($uri->input, $id->toUri());
-            $this->assertNotSame($id->toString(), $id->toUri());
+            $id = RedirectId::fromUri($uri['input']);
+            $this->assertSame($uri['output'], $id->toString());
+            $this->assertSame($uri['input'], $id->toUri());
         }
     }
 
