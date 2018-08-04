f2bc8c72617738d4169d28eb86e589e604abdc8c
Eric Jacobson <contact@efjacobson.com>
2018-08-03 17:45:59 -0700

    compiled

---
+++ build/php/src/Triniti/Schemas/Curator/Mixin/SyncTeaser/SyncTeaser.php
@@ -0,0 +1,9 @@
+<?php
+// @link http://schemas.triniti.io/json-schema/triniti/curator/mixin/sync-teaser/latest.json#
+namespace Triniti\Schemas\Curator\Mixin\SyncTeaser;
+
+use Gdbots\Schemas\Pbjx\Mixin\Command\Command;
+
+interface SyncTeaser extends Command
+{
+}
---
+++ build/php/src/Triniti/Schemas/Curator/Mixin/SyncTeaser/SyncTeaserV1.php
@@ -0,0 +1,7 @@
+<?php
+// @link http://schemas.triniti.io/json-schema/triniti/curator/mixin/sync-teaser/1-0-0.json#
+namespace Triniti\Schemas\Curator\Mixin\SyncTeaser;
+
+interface SyncTeaserV1 extends SyncTeaser
+{
+}
