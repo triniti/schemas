ad96ca9e9ef9c3e1e18955b3c47cc7c15dcd2256
Greg Brown <greg@12dnetworks.com>
2019-08-13 18:54:39 -0700

    add purge-cache command and duration to audio and video asset - pbjc

--- build/js/src/triniti/dam/mixin/video-asset/VideoAssetV1Mixin.js
+++ build/js/src/triniti/dam/mixin/video-asset/VideoAssetV1Mixin.js
@@ -1,12 +1,27 @@
-// @link http://schemas.triniti.io/json-schema/triniti/dam/mixin/video-asset/1-0-0.json#
+// @link http://schemas.triniti.io/json-schema/triniti/dam/mixin/video-asset/1-0-1.json#
+import Fb from '@gdbots/pbj/FieldBuilder';
 import Mixin from '@gdbots/pbj/Mixin';
 import SchemaId from '@gdbots/pbj/SchemaId';
+import T from '@gdbots/pbj/types';
 
 export default class VideoAssetV1Mixin extends Mixin {
   /**
    * @returns {SchemaId}
    */
   getId() {
-    return SchemaId.fromString('pbj:triniti:dam:mixin:video-asset:1-0-0');
+    return SchemaId.fromString('pbj:triniti:dam:mixin:video-asset:1-0-1');
+  }
+
+  /**
+   * @returns {Field[]}
+   */
+  getFields() {
+    return [
+      /*
+       * Duration of the video in seconds.
+       */
+      Fb.create('duration', T.SmallIntType.create())
+        .build(),
+    ];
   }
 }
--- build/php/src/Triniti/Schemas/Dam/Mixin/VideoAsset/VideoAssetV1.php
+++ build/php/src/Triniti/Schemas/Dam/Mixin/VideoAsset/VideoAssetV1.php
@@ -1,5 +1,5 @@
 <?php
-// @link http://schemas.triniti.io/json-schema/triniti/dam/mixin/video-asset/1-0-0.json#
+// @link http://schemas.triniti.io/json-schema/triniti/dam/mixin/video-asset/1-0-1.json#
 namespace Triniti\Schemas\Dam\Mixin\VideoAsset;
 
 interface VideoAssetV1 extends VideoAsset
