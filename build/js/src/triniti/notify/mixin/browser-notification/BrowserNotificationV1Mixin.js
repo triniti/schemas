9a60daf817e0b140ee5863585916ffb5466051f6
Arun Karnati <arun.karnati@gmail.com>
2019-09-17 12:40:12 -0700

    composer pbjc

--- build/js/src/triniti/notify/mixin/browser-notification/BrowserNotificationV1Mixin.js
+++ build/js/src/triniti/notify/mixin/browser-notification/BrowserNotificationV1Mixin.js
@@ -1,6 +1,8 @@
 // @link http://schemas.triniti.io/json-schema/triniti/notify/mixin/browser-notification/1-0-0.json#
+import Fb from '@gdbots/pbj/FieldBuilder';
 import Mixin from '@gdbots/pbj/Mixin';
 import SchemaId from '@gdbots/pbj/SchemaId';
+import T from '@gdbots/pbj/types';
 
 export default class BrowserNotificationV1Mixin extends Mixin {
   /**
@@ -8,5 +10,21 @@ export default class BrowserNotificationV1Mixin extends Mixin {
    */
   getId() {
     return SchemaId.fromString('pbj:triniti:notify:mixin:browser-notification:1-0-0');
+  }
+
+  /**
+   * @returns {Field[]}
+   */
+  getFields() {
+    return [
+      /*
+       * @link https://firebase.google.com/docs/cloud-messaging/ios/topic-messaging
+       * FCM topic messaging allows you to send a message to multiple devices that have opted in to a particular topic.
+       */
+      Fb.create('fcm_topics', T.StringType.create())
+        .asASet()
+        .pattern('^[\\w\\/\\.:~%#-]+$')
+        .build(),
+    ];
   }
 }
