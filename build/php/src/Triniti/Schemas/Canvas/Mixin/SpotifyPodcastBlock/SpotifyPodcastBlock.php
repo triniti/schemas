e6c6e29945f56d1867f9127d86e3007bec674990
Eric Jacobson <contact@efjacobson.com>
2019-10-31 12:42:24 -0700

    remove spotify-podcast-block build files

--- build/php/src/Triniti/Schemas/Canvas/Mixin/SpotifyPodcastBlock/SpotifyPodcastBlock.php
+++
@@ -1,9 +0,0 @@
-<?php
-// @link http://schemas.triniti.io/json-schema/triniti/canvas/mixin/spotify-podcast-block/latest.json#
-namespace Triniti\Schemas\Canvas\Mixin\SpotifyPodcastBlock;
-
-use Triniti\Schemas\Canvas\Mixin\Block\Block;
-
-interface SpotifyPodcastBlock extends Block
-{
-}
