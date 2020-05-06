4366c03802714047296af8a9d62fddaf23ec272b
Eric Jacobson <contact@efjacobson.com>
2020-05-06 13:04:43 -0700

    compiled

---
+++ build/php/src/Triniti/Schemas/Ovp/Mixin/Transcribable/TranscribableV1.php
@@ -0,0 +1,7 @@
+<?php
+// @link http://schemas.triniti.io/json-schema/triniti/ovp/mixin/transcribable/1-0-0.json#
+namespace Triniti\Schemas\Ovp\Mixin\Transcribable;
+
+interface TranscribableV1 extends Transcribable
+{
+}
