13f1088a5bb39924c79b1ddcaf61b36fd7e4bdca
Donte Burney <donte.burney@fox.com>
2024-10-01 08:39:56 -1000

    modified browser schemas

--- build/php/src/manifests/triniti/notify/mixin/browser-notification/v1.php
+++ build/php/src/manifests/triniti/notify/mixin/browser-notification/v1.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
  * @link https://github.com/gdbots/pbjc-php
  *
  * Returns an array of curies using mixin "triniti:notify:mixin:browser-notification:v1"
- * @link http://schemas.triniti.io/json-schema/triniti/notify/mixin/browser-notification/1-0-2.json#
+ * @link http://schemas.triniti.io/json-schema/triniti/notify/mixin/browser-notification/1-0-3.json#
  */
 
 return [
